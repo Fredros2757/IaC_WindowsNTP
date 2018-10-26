ba5de68268ee619ef444cbbbf87e8dcac5f78d89
Morten <mortenbjerke95@gmail.com>
2018-10-26 14:51:27 +0000

    updated ruby hash syntax in compliance with new standards in ruby version 1.9

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe 'windowstime', :type => :class do
+describe 'windowstime', type: :class do
   context 'with default values for all parameters' do
-    it { is_expected.to compile }
+    it {  is_expected.to compile }
   end
 end
