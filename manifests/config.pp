c5bb605333e032cdaa3d81832e0d596c3dae045b
Morten <mortenbjerke95@gmail.com>
2018-11-01 17:04:08 +0000

    Updated class structure; now using config.pp, init.pp and services.pp. Updated namespace on variables in line with new classes. Also changed some values into variables, defaults added to common.yaml

---
+++ manifests/config.pp
@@ -0,0 +1,106 @@
+#PDK warnings go away
+class windowstime::config {
+
+  if $windowstime::logging {
+    exec {'Enabling debug log':
+      command  => "w32tm /debug /enable /file:${windowstime::debugpath}\
+      /size:${windowstime::debugsize} /entries:${windowstime::debugentryfirst}-${windowstime::debugentrylast}",
+      provider => powershell,
+      path     => 'c:/windows/system32',
+    }
+  }
+
+  if !$windowstime::logging {
+    exec {'Disabling debug log':
+      command  => 'w32tm /debug /disable',
+      provider => powershell,
+      path     => 'c:/windows/system32',
+    }
+  }
+
+  exec {'Time culture':
+    command  => "Set-Culture ${windowstime::timeculture}",
+    provider => powershell,
+    path     => 'c:/windows/system32',
+  }
+
+  $regvalue = maptoreg($windowstime::servers)
+  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\Type':
+    ensure => present,
+    type   => string,
+    data   => 'NTP'
+  }
+
+  registry_value { 'HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters\NtpServer':
+    ensure => present,
+    type   => string,
+    data   => $regvalue,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxPosPhaseCorrection':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::max_pos_phase_correction,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxNegPhaseCorrection':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::max_neg_phase_correction,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\TimeProviders\\NtpClient\\SpecialPollInterval':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::special_poll_interval,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MaxPollInterval':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::max_poll_interval,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\MinPollInterval':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::min_poll_interval,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\LargePhaseOffset':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::large_phase_offset,
+    notify => Service['w32time'],
+  }
+
+  registry_value { 'HKLM\\SYSTEM\\CurrentControlSet\\Services\\W32Time\\Config\\UpdateInterval':
+    ensure => present,
+    type   => 'dword',
+    data   => $windowstime::update_interval,
+    notify => Service['w32time'],
+  }
+
+  exec { 'c:/Windows/System32/w32tm.exe /resync':
+    refreshonly => true,
+  }
+
+  if $::timezone {
+    validate_re($windowstime::timezone, $windowstime::timezones, 'The specified string is not a valid Timezone')
+    if $windowstime::timezone != $facts['timezone'] {
+      $system32dir = $facts['os']['windows']['system32']
+      exec { 'Sets specified timezone':
+        command   => "tzutil /s ${windowstime::timezone}",
+        provider  => powershell,
+        path      => system32dir,
+        logoutput => true,
+      }
+    }
+  }
+}
---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+#
+#This class handles the w32time service
+#
+class windowstime::service {
+  service { 'w32time':
+    ensure   => $windowstime::service_ensure,
+    enable   => $windowstime::service_enable,
+    notify   => $windowstime::service_notify,
+    provider => $windowstime::service_provider,
+  }
+}
