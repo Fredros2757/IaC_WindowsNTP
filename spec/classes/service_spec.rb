5781fe86a9da2cafa23628cd04512c996225cd09
Fredrik <fredrikrostad@gmail.com>
2018-11-02 14:07:12 +0000

    Made core for PDK testing, but still getting errors. Minor fix to stack2 template.

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-describe 'windowstime' do
+describe 'windowstime::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:node_params) do
+        {
+          'windowstime::service_ensure' => 'running',
+        }
+      end
 
       it { is_expected.to compile }
     end
